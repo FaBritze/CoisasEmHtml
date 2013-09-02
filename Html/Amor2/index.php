e9db8fda22b7e26622cf0425c511615a5e3c673b
FaBritze <fabricio.e.reche@gmail.com>
2013-09-02 15:47:59 -0300

    Adicionada segunda mensagem para o meu amor

---
+++ Html/Amor2/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>..Te amo linda!!!...</title>
+<link href="css/stl.css" type="text/css" rel="stylesheet" />
+</head>
+
+<body>
+	<h1>Rosas para o meu amor...</h1>
+    <p>Não há palavras para descrever meu amor para ti...</p>
+    <p>É com você que eu divido todos os meus sentimentos, minhas dúvidas, tristezas, alegrias, batalhas e tudo mais que tiver...</p>
+    <p>É contigo que eu viverei para sempre, foi tu que Deus escolheu para mim, e Ele sabe muito bem o que faz...</p>
+    <p>Este é meu pequeno presente, humilde e simples, mas que te entrego de coração, pois não queria que essa data passasse em branco...</p>
+    <p>Estes 10 meses contigo foram maravilhosos, aprendi a me superar, a enfrentar desafios, a ser ousado, a ser alguém melhor...</p>
+    <p>E continuamos nossa jornada, sempre crescendo nosso amor e nossa união com Deus, para sermos, como sempre, o casal mais apaixonado do mundo...</p>
+    <p>Afinal... Assim é só no começo né? Hehehehhehe</p>
+    <h3>EU TE AMO MEU AMOR! FELIZ 10 MESES DE CASAMENTO!
+    	<label>De seu eterno amado... Fabricio Eduardo Reche</label>
+    </h3>
+</body>
+</html>
